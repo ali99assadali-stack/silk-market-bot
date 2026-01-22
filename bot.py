1679ee9939c448ffe626cb73688159680a78bb30
ali99assadali-stack <ali99assadali@gmail.com>
2026-01-22 04:16:45 +0200

    Integrate Flask web server for bot status
    Added a Flask web server to the bot for status reporting.

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 import time
 import os
 import threading
+from flask import Flask
 from datetime import datetime
 from flask import Flask
 
@@ -298,3 +299,16 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # ================== Render Web Server ==================
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_flask).start()
